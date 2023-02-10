8d77bcee7c6ad7736ca665df89d2e42f1fea5a8b
Lars Skaret <lars.skaret@gmail.com>
2023-02-10 14:33:36 +0000

    finished project

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,5 +1,12 @@
 {{ config(materialized='view') }}
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+  from {{ source('staging','green_tripdata') }}
+  where vendorid is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -27,12 +34,11 @@ select
     cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type, 
-    {{ get_payment_type_description('payment_type') }} as payment_type_description,
+    cast(payment_type as integer) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
-    
-from {{ source('staging','green_tripdata') }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
